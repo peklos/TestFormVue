214a65a2780e319348e002422b944420cb879ea8
peklos <vladoserov727@mail.ru>
2025-07-06 17:14:17 +0300

    init2

--- src/components/RowInTable.vue
+++ src/components/RowInTable.vue
@@ -131,7 +131,7 @@
 </template>
 
 <script setup lang="ts">
-import { ref, watch } from "vue";
+import { ref } from "vue";
 import { useUsersStore } from "../stores/usersStore.ts";
 
 const isPasswordVisible = ref(false);
